137387d1c4fb951d302ca79a581031234ace79fc
jonathan YHWH <alphajorider@gmail.com>
2025-08-18 18:11:42 +0200

    vilebrequin.py

---
+++ backend/pieces/vilebrequin.py
@@ -0,0 +1,5 @@
+# backend\pieces\vilebrequin.py
+"""
+  * **Rayon de manivelle** $r = S/2$.
+  * Diamètre de maneton et contrepoids dimensionnés sur les efforts alternatifs $\propto B^2$ et l’inertie (fonction de S et du régime).
+"""
