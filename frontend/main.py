efe873738992b720506a23817fe7c6d8da25cee5
jonathan YHWH <93312510+jonathandu07@users.noreply.github.com>
2026-05-16 00:41:22 +0200

    Update main.py

--- frontend/main.py
+++ frontend/main.py
@@ -14,8 +14,12 @@ from typing import Any, Dict, List, Optional
 
 # CONFIGURATION DU PATH
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+FRONTEND_DIR = os.path.abspath(os.path.dirname(__file__))
+
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
+if FRONTEND_DIR not in sys.path:
+    sys.path.insert(0, FRONTEND_DIR)
 
 KIVY_HOME = os.path.join(BASE_DIR, ".kivy")
 os.makedirs(KIVY_HOME, exist_ok=True)
